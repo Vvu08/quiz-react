539faacf2f05e463e409b2768ddd7d6874191cb9
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-11-30 18:22:58 +0200

    back to tests btn bug fix

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,7 +3,7 @@ export default function Result ({score}) {
       <>
       <h1 className='result-title'>You scored {score} points!</h1>
       <img className='result-img' src={process.env.PUBLIC_URL + '/images/result.png'} alt='results'></img>
-      <a className='back-btn'  href='/'>
+      <a className='back-btn'  href={ process.env.PUBLIC_URL }>
         <h2 className='text'>Back to tests</h2>
       </a>
       </>
