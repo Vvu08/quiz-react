c7ee85e7614cd136cc4857180329b049bc116a43
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-11-30 18:20:50 +0200

    image bug fix

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,7 +2,7 @@ export default function Result ({score}) {
     return (
       <>
       <h1 className='result-title'>You scored {score} points!</h1>
-      <img className='result-img' src={'./images/result.png'} alt='results'></img>
+      <img className='result-img' src={process.env.PUBLIC_URL + '/images/result.png'} alt='results'></img>
       <a className='back-btn'  href='/'>
         <h2 className='text'>Back to tests</h2>
       </a>
