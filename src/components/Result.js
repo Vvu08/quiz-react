c6804e9f0883072bdba081374acad4a55cef8f48
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-11-30 18:25:38 +0200

    back to tests btn bug fix

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,13 @@
 export default function Result ({score}) {
+    const refreshPage = () =>  {
+      window.location.reload();
+    }
+
     return (
       <>
       <h1 className='result-title'>You scored {score} points!</h1>
       <img className='result-img' src={process.env.PUBLIC_URL + '/images/result.png'} alt='results'></img>
-      <a className='back-btn'  href={ process.env.PUBLIC_URL }>
+      <a className='back-btn'  onClick={refreshPage} >
         <h2 className='text'>Back to tests</h2>
       </a>
       </>
